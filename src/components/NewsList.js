b02bb4586bc8a6f4c8f8d45fa6233559eceecbce
paktan <pakan@gmail.com>
2019-10-07 18:10:25 +0900

    category add

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -23,7 +23,7 @@ const sampleArticle = {
   urlToImage: 'https://via.placeholder.com/160',
 }
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
   const [articles, setArticles] = useState(null)
   const [loading, setLoading] = useState(false)
 
@@ -31,8 +31,9 @@ const NewsList = () => {
     const fetchData = async () => {
       setLoading(true)
       try {
+        const query = category === 'all' ? '' : `&category=${category}`
         const response = await axios.get(
-          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=56a9be0c23f94899b8e94b892e9081df',
+          'https://newsapi.org/v2/top-headlines?country=kr'+query+"&apiKey=56a9be0c23f94899b8e94b892e9081df",
         )
         setArticles(response.data.articles)
       } catch (error) {
@@ -41,7 +42,7 @@ const NewsList = () => {
       setLoading(false)
     }
     fetchData()
-  }, [])
+  }, [category])
 
   if (loading) {
     return <NewsListBlock>대기중...</NewsListBlock>
