a8977be210c669884ab6958ac5396c3a5f69eb71
paktan <pakan@gmail.com>
2019-10-07 16:49:54 +0900

    axios async await

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import axios from "axios"
 
 const App = () => {
   const [data, setData] = useState(null)
-  const onClick = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos/1").then(response => {
+  const onClick = async () => {
+    try {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/todos/1")
       setData(response.data)
-    })
+    } catch (error) {
+      console.log(error)
+    }
   }
   return (
     <div >
