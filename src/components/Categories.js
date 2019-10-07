2d49df86fb769025149dddc6eab4e2f8792cbbc4
paktan <pakan@gmail.com>
2019-10-07 18:33:09 +0900

    router add

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled, {css} from 'styled-components'
+import {NavLink} from "react-router-dom"
 
 const categories = [
   {
@@ -42,7 +43,7 @@ margin:@ auto;
     overflow-x: auto;
 }`
 
-const Category = styled.div`
+const Category = styled(NavLink)`
   font-size: 1.125.rem;
   cursor: pointer;
   white-space: pre;
@@ -54,15 +55,14 @@ const Category = styled.div`
     color: $495057;
   }
 
-  ${props =>
-    props.active && css`
-      font-weight: 600;
-      border-bottom: 2px solid #22b8cf;
+  &.active {
+      font-weight:600;
+      border-bottom:2px solid #22b8cf;
       color: #22b8cf;
-      &:hover {
+      &:hover{
         color: #3bc9db;
-      }
-    `}
+    }
+  }
 
   &+& {
     margin-left: 1rem;
@@ -75,8 +75,9 @@ const Categories = ({ onSelect, category }) => {
       {categories.map(c => (
         <Category
           key={c.name}
-          active={category === c.name}
-          onClick={() => onSelect(c.name)}
+          activeClassName="active"
+          exact={c.name === "all"}
+          to={c.name === "all" ? "/" : `/${c.name}`}
         >
           {c.text}
         </Category>
