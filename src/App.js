8900c222ba60a95e70611b80070afd73a39c96e0
paktan <pakan@gmail.com>
2019-10-07 17:29:59 +0900

    news viewer headline news get

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import React, {useState} from 'react';
-import axios from "axios"
+import React, { useState } from 'react'
+import NewsList from './components/NewsList'
 
 const App = () => {
-  const [data, setData] = useState(null)
-  const onClick = async () => {
-    try {
-      const response = await axios.get("https://jsonplaceholder.typicode.com/todos/1")
-      setData(response.data)
-    } catch (error) {
-      console.log(error)
-    }
-  }
   return (
-    <div >
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}/>}
-    </div>
-  );
+    <NewsList></NewsList>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/NewsList.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import NewsItem from './NewsItem'
+import axios from 'axios'
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 769px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`
+
+const sampleArticle = {
+  title: '제목',
+  description: '내용',
+  url: 'https://google.com',
+  urlToImage: 'https://via.placeholder.com/160',
+}
+
+const NewsList = () => {
+  const [articles, setArticles] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true)
+      try {
+        const response = await axios.get(
+          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=56a9be0c23f94899b8e94b892e9081df',
+        )
+        setArticles(response.data.articles)
+      } catch (error) {
+        console.log(error)
+      }
+      setLoading(false)
+    }
+    fetchData()
+  }, [])
+
+  if (loading) {
+    return <NewsListBlock>대기중...</NewsListBlock>
+  }
+  if (!articles) {
+    return null
+  }
+  return (
+    <NewsListBlock>
+      {articles.map(article => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  )
+}
+
+export default NewsList
